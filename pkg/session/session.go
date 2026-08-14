9be7ee06f8cda7ad98a6d145b8c8ca0e58d373d4
mikestefanello <552328+mikestefanello@users.noreply.github.com>
2026-08-13 20:22:16 -0400

    Avoid error loading sessions when decoding fails.

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -8,20 +8,25 @@ import (
 	"github.com/mikestefanello/pagoda/pkg/context"
 )
 
-// ErrStoreNotFound indicates that the session store was not present in the context
+// ErrStoreNotFound indicates that the session store was not present in the context.
 var ErrStoreNotFound = errors.New("session store not found")
 
-// Get returns a session
+// Get returns a session.
 func Get(ctx echo.Context, name string) (*sessions.Session, error) {
 	s := ctx.Get(context.SessionKey)
 	if s == nil {
 		return nil, ErrStoreNotFound
 	}
 	store := s.(sessions.Store)
-	return store.Get(ctx.Request(), name)
+
+	// Errors are ignored because it's possible the cookie value was invalid or the encryption key
+	// changed since the cookie was written. A usable session is always returned even if a new one
+	// has to be created.
+	sess, _ := store.Get(ctx.Request(), name)
+	return sess, nil
 }
 
-// Store sets the session storage in the context
+// Store sets the session storage in the context.
 func Store(ctx echo.Context, store sessions.Store) {
 	ctx.Set(context.SessionKey, store)
 }
--- pkg/session/session_test.go
+++ pkg/session/session_test.go
@@ -8,16 +8,48 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetStore(t *testing.T) {
+	const sessName = "test"
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	ctx := e.NewContext(req, httptest.NewRecorder())
-	_, err := Get(ctx, "test")
+
+	// No store is set on the context yet.
+	_, err := Get(ctx, sessName)
 	assert.Equal(t, ErrStoreNotFound, err)
 
+	// Set up the store.
 	Store(ctx, sessions.NewCookieStore([]byte("secret")))
-	_, err = Get(ctx, "test")
-	assert.NoError(t, err)
+	sess, err := Get(ctx, sessName)
+	require.NoError(t, err)
+
+	// Save session data.
+	sess.Values["a"] = 1
+	err = sess.Save(ctx.Request(), ctx.Response())
+	require.NoError(t, err)
+
+	// Ensure the session data can be fetched.
+	sess, err = Get(ctx, sessName)
+	require.NoError(t, err)
+	assert.Equal(t, sess.Values["a"], 1)
+
+	// Create a new request with the session cookie.
+	cookie := ctx.Response().Header().Get("Set-Cookie")
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Add("Cookie", cookie)
+	ctx = e.NewContext(req, httptest.NewRecorder())
+
+	// Change the encryption key and ensure no error is returned.
+	Store(ctx, sessions.NewCookieStore([]byte("newSecret")))
+	_, err = Get(ctx, sessName)
+	require.NoError(t, err)
+
+	// The session data shouldn't be present now.
+	sess, err = Get(ctx, sessName)
+	require.NoError(t, err)
+	_, exists := sess.Values["a"]
+	assert.False(t, exists)
 }
